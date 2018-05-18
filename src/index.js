3a590c1950990a43d4d1642696356846331d437b
aymfx <1095731371@qq.com>
2018-05-18 14:55:27 +0800

    管理资源2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import _ from 'lodash';
 import './scc/style.css';
-import imgsrc from './images/a.jpg'
+import printMe from './js/print.js'
 function component() {
     var element = document.createElement('div');
-
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    var btn = document.createElement('button');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    var myIcon = new Image();
-    myIcon.src = imgsrc;
-    // myIcon.width=100+'px';
-    element.appendChild(myIcon);
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     return element;
 }
-
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-plugin-manifest');
 
 module.exports = {
     mode: "development",
-    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    entry: {
+        app:'./src/index.js',
+        print:'./src/js/print.js'
+    },
     output: {
-        filename: 'index.js',
+        filename: '[name].[hash].js',
         path: path.resolve(__dirname, 'dist')
+    },
+    devServer:{
+        contentBase:'./dist',
     },
     module:{
         rules:[
@@ -29,5 +39,13 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title:'test',
+            template:'index.html'
+        }),
+        new CleanWebpackPlugin(['dist']),
+        new ManifestPlugin()
+    ]
 };
